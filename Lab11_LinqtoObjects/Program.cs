00746f239ac088573f5285d5ded0019891e163ff
Alexandre Agustini <alexandre.agustini@gmail.com>
2017-05-03 15:48:33 -0300

    Corrigido exercício de groupby

--- Lab11_LinqtoObjects/Program.cs
+++ Lab11_LinqtoObjects/Program.cs
@@ -23,134 +23,134 @@ namespace Lab11_LinqtoObjects
 
             };
             
-            #region #demo...
-            //Consulta tradiconal
-            List<Pessoa> resultado1 = new List<Pessoa>();
-            foreach (Pessoa p in pessoas)
-            {
-                if (p.Casada)
-                    resultado1.Add(p);
-            }
-            Console.WriteLine("Pessoas casadas, consulta tradiconal:");
-            foreach (Pessoa p in resultado1)
-            {
-                Console.WriteLine(p.Nome);
-            }
+            //#region #demo...
+            ////Consulta tradiconal
+            //List<Pessoa> resultado1 = new List<Pessoa>();
+            //foreach (Pessoa p in pessoas)
+            //{
+            //    if (p.Casada)
+            //        resultado1.Add(p);
+            //}
+            //Console.WriteLine("Pessoas casadas, consulta tradiconal:");
+            //foreach (Pessoa p in resultado1)
+            //{
+            //    Console.WriteLine(p.Nome);
+            //}
 
-            //Consulta LINQ
-            var resultado2 = from p in pessoas
-                             where p.Casada
-                             select p;
-            Console.WriteLine("\nPessoas casadas, consulta linq:");
-            foreach (Pessoa p in resultado2)
-            {
-                Console.WriteLine(p.Nome);
-            }
+            ////Consulta LINQ
+            //var resultado2 = from p in pessoas
+            //                 where p.Casada
+            //                 select p;
+            //Console.WriteLine("\nPessoas casadas, consulta linq:");
+            //foreach (Pessoa p in resultado2)
+            //{
+            //    Console.WriteLine(p.Nome);
+            //}
 
-            Console.WriteLine("\nPessoas casadas, consulta linq (method syntax):");
-            var resultado3 = pessoas.Where(p => p.Casada);
-            foreach (Pessoa p in resultado3)
-            {
-                Console.WriteLine(p.Nome);
-            }
+            //Console.WriteLine("\nPessoas casadas, consulta linq (method syntax):");
+            //var resultado3 = pessoas.Where(p => p.Casada);
+            //foreach (Pessoa p in resultado3)
+            //{
+            //    Console.WriteLine(p.Nome);
+            //}
 
-            // para obter uma "lista" a partir da consulta linq
-            var resultado4 = (from p in pessoas
-                              where p.Casada
-                              select p).ToList();
-
-
-            Console.WriteLine("\nPessoas casadas que nasceram após 01/01/1980");
-            var resultado5 = (from p in pessoas
-                              where p.Casada && p.DataNascimento >= new DateTime(1980, 1, 1)
-                              select p).ToList();
-
-            // ups! forçando um pouco a barra...
-            resultado5.ForEach(p => Console.WriteLine(p.Nome + " " + p.DataNascimento));
+            //// para obter uma "lista" a partir da consulta linq
+            //var resultado4 = (from p in pessoas
+            //                  where p.Casada
+            //                  select p).ToList();
 
 
-            Console.WriteLine("\nProjeção sobre o nome das pessoas casadas");
-            var resultado6 = from p in pessoas
-                             where p.Casada
-                             select p.Nome;
+            //Console.WriteLine("\nPessoas casadas que nasceram após 01/01/1980");
+            //var resultado5 = (from p in pessoas
+            //                  where p.Casada && p.DataNascimento >= new DateTime(1980, 1, 1)
+            //                  select p).ToList();
 
-            foreach (String s in resultado6)
-            {
-                Console.WriteLine(s);
-            }
-
-            Console.WriteLine("\nSoteiros em uma lista de objetos anonimos..");
-            var resultado7 = (from p in pessoas
-                              where !p.Casada
-                              select new { p.Nome, p.DataNascimento }).ToList();
-
-            foreach (var elem in resultado7)
-            {
-                Console.WriteLine(elem.Nome + " " + elem.DataNascimento);
-            }
-
-            Console.WriteLine("\nPessoas agrupadas pelo estado civil");
-            var resultado8 = from p in pessoas
-                             group p by p.Casada;
-            foreach (var g in resultado8)
-            {
-                Console.WriteLine($"casado:{g.Key}");
-                foreach (var p in g)
-                {
-                    Console.WriteLine(p);
-                }
-            }
-
-            Console.WriteLine("\nPessoa mais nova");
-            var resultado9 = pessoas.Min(p => p.DataNascimento);
-            //Console.WriteLine("Consulta 9:");
-            //Console.WriteLine(resultado9);
-
-            var resultado10 = (from p in pessoas
-                               where p.DataNascimento == resultado9
-                               select p.Nome).ToList();
-
-            resultado10.ForEach(x => Console.WriteLine(x));
-
-            Console.ReadLine();
-            #endregion
+            //// ups! forçando um pouco a barra...
+            //resultado5.ForEach(p => Console.WriteLine(p.Nome + " " + p.DataNascimento));
 
 
-            Console.WriteLine("\n>>> Nome e data de nascimento das pessoas casadas");
-            int cont = 0;
-            var r3 = from p in pessoas
-                     where p.Casada
-                     select new { Id = ++cont,
-                                 p.Nome,
-                                 p.DataNascimento };  // objeto anonimo
+            //Console.WriteLine("\nProjeção sobre o nome das pessoas casadas");
+            //var resultado6 = from p in pessoas
+            //                 where p.Casada
+            //                 select p.Nome;
 
-            Console.WriteLine("contador...: " + cont);          // query ainda não foi executada...
+            //foreach (String s in resultado6)
+            //{
+            //    Console.WriteLine(s);
+            //}
 
-            foreach (var p in r3)
-                Console.WriteLine(p.Id + ": " + p.Nome + " " + p.DataNascimento);
-            Console.WriteLine();
+            //Console.WriteLine("\nSoteiros em uma lista de objetos anonimos..");
+            //var resultado7 = (from p in pessoas
+            //                  where !p.Casada
+            //                  select new { p.Nome, p.DataNascimento }).ToList();
 
-            Console.WriteLine("contador...: " + cont);          // incrementado na consulta
-            Console.ReadKey();
+            //foreach (var elem in resultado7)
+            //{
+            //    Console.WriteLine(elem.Nome + " " + elem.DataNascimento);
+            //}
 
-            Console.WriteLine(">>> Pessoas ordenadas pelo nome");
-            var r4 = from p in pessoas
-                     orderby p.Nome
-                     select p;
-            foreach (var p in r4)
-                Console.WriteLine(p.Nome);
-            Console.WriteLine();
+            //Console.WriteLine("\nPessoas agrupadas pelo estado civil");
+            //var resultado8 = from p in pessoas
+            //                 group p by p.Casada;
+            //foreach (var g in resultado8)
+            //{
+            //    Console.WriteLine($"casado:{g.Key}");
+            //    foreach (var p in g)
+            //    {
+            //        Console.WriteLine(p);
+            //    }
+            //}
 
-            Console.WriteLine(">>> Pessoas ordenadas pelo nome - extension methods");
-            //IEnumerable<string> r4e =  pessoas.Where( p => p.Casada ).OrderBy( p => p.Nome ).Select( p => p.Nome.ToUpper() );
-            IEnumerable<string> r4e = pessoas.Where(p => p.Casada)
-                                             .OrderBy(p => p.Nome)
-                                             .Select(p => p.Nome.ToUpper());
+            //Console.WriteLine("\nPessoa mais nova");
+            //var resultado9 = pessoas.Min(p => p.DataNascimento);
+            ////Console.WriteLine("Consulta 9:");
+            ////Console.WriteLine(resultado9);
 
-            foreach (var nome in r4e)
-                Console.WriteLine(nome);
-            Console.WriteLine();
-            Console.ReadKey();
+            //var resultado10 = (from p in pessoas
+            //                   where p.DataNascimento == resultado9
+            //                   select p.Nome).ToList();
+
+            //resultado10.ForEach(x => Console.WriteLine(x));
+
+            //Console.ReadLine();
+            //#endregion
+
+
+            //Console.WriteLine("\n>>> Nome e data de nascimento das pessoas casadas");
+            //int cont = 0;
+            //var r3 = from p in pessoas
+            //         where p.Casada
+            //         select new { Id = ++cont,
+            //                     p.Nome,
+            //                     p.DataNascimento };  // objeto anonimo
+
+            //Console.WriteLine("contador...: " + cont);          // query ainda não foi executada...
+
+            //foreach (var p in r3)
+            //    Console.WriteLine(p.Id + ": " + p.Nome + " " + p.DataNascimento);
+            //Console.WriteLine();
+
+            //Console.WriteLine("contador...: " + cont);          // incrementado na consulta
+            //Console.ReadKey();
+
+            //Console.WriteLine(">>> Pessoas ordenadas pelo nome");
+            //var r4 = from p in pessoas
+            //         orderby p.Nome
+            //         select p;
+            //foreach (var p in r4)
+            //    Console.WriteLine(p.Nome);
+            //Console.WriteLine();
+
+            //Console.WriteLine(">>> Pessoas ordenadas pelo nome - extension methods");
+            ////IEnumerable<string> r4e =  pessoas.Where( p => p.Casada ).OrderBy( p => p.Nome ).Select( p => p.Nome.ToUpper() );
+            //IEnumerable<string> r4e = pessoas.Where(p => p.Casada)
+            //                                 .OrderBy(p => p.Nome)
+            //                                 .Select(p => p.Nome.ToUpper());
+
+            //foreach (var nome in r4e)
+            //    Console.WriteLine(nome);
+            //Console.WriteLine();
+            //Console.ReadKey();
 
 
             //Console.WriteLine(">>> Pessoas ordenadas inversamente pelo nome");
@@ -161,53 +161,79 @@ namespace Lab11_LinqtoObjects
             //    Console.WriteLine(p.Nome);
             //Console.WriteLine();
 
-            #region Console.WriteLine(">>> Pessoas ordenadas pela data de nascimento e nome");
-            var r6 = from p in pessoas
-                     orderby p.DataNascimento, p.Nome
-                     select p;
-            foreach (var p in r6)
-                Console.WriteLine(p.DataNascimento + " " + p.Nome);
-            Console.WriteLine();
-            #endregion
+            //#region Console.WriteLine(">>> Pessoas ordenadas pela data de nascimento e nome");
+            //var r6 = from p in pessoas
+            //         orderby p.DataNascimento, p.Nome
+            //         select p;
+            //foreach (var p in r6)
+            //    Console.WriteLine(p.DataNascimento + " " + p.Nome);
+            //Console.WriteLine();
+            //#endregion
 
-            #region  Console.WriteLine(">>> Pessoas agrupadas pelo estado civil, com quantidade e conversão do boolean");
-            var r7 = from p in pessoas
-                     group p by p.Casada;
-            foreach (var g in r7)
+            //#region  Console.WriteLine(">>> Pessoas agrupadas pelo estado civil, com quantidade e conversão do boolean");
+            //var r7 = from p in pessoas
+            //         group p by p.Casada;
+            //foreach (var g in r7)
+            //{
+
+            //    Console.WriteLine("Categoria: " + (g.Key ? "CASADAS" : "SOLTEIRAS") + " quantidade: " + g.Count());
+            //    foreach (var p in g)
+            //        Console.WriteLine("  " + p.Nome);
+            //}
+            //Console.WriteLine();
+            //#endregion
+
+            //#region  Console.WriteLine(">>> Projeção de pessoas agrupadas pelo estado civil");
+            //var r8 = from p in pessoas
+            //         group p by p.Casada
+            //             into grupoPessoas
+            //             select new { Categoria = grupoPessoas.Key, Pessoas = grupoPessoas, NroPessoas = grupoPessoas.Count() };
+
+            //foreach (var g in r8)
+            //{
+            //    Console.WriteLine("Categoria: " + g.Categoria + " Quantidade: " + g.NroPessoas);
+
+            //    foreach (var p in g.Pessoas)
+            //        Console.WriteLine("  " + p.Nome);
+            //}
+            //Console.WriteLine();
+            //#endregion
+
+            //#region Console.WriteLine("\n>>> Total de pessoas casadas");
+            //var r9 = (from p in pessoas
+            //          where p.Casada
+            //          select p).Count();
+            //Console.WriteLine(r9);
+            //Console.WriteLine();
+            //#endregion
+
+
+            #region  Console.WriteLine(">>> Pessoas agrupadas pelo mes de nascimento, ordenado");
+            var r27 = (from p in pessoas
+                      group p by p.DataNascimento.Month).ToList();
+
+
+            r27.ForEach(e =>
+            {
+                Console.WriteLine("chave: " + e.Key);
+                 e.ToList().ForEach(p=> Console.WriteLine("\tnome: " + p.Nome)); 
+            });
+            foreach (var g in r27.OrderBy(p=>p.Key))
             {
 
-                Console.WriteLine("Categoria: " + (g.Key ? "CASADAS" : "SOLTEIRAS") + " quantidade: " + g.Count());
-                foreach (var p in g)
+                Console.WriteLine("Mes: " + (g.Key) + 
+                                  " quantidade: " + g.Count());
+
+                foreach (var p in g.OrderBy(p=>p.Nome))
+
+     
                     Console.WriteLine("  " + p.Nome);
             }
             Console.WriteLine();
             #endregion
 
-            #region  Console.WriteLine(">>> Projeção de pessoas agrupadas pelo estado civil");
-            var r8 = from p in pessoas
-                     group p by p.Casada
-                         into grupoPessoas
-                         select new { Categoria = grupoPessoas.Key, Pessoas = grupoPessoas, NroPessoas = grupoPessoas.Count() };
 
-            foreach (var g in r8)
-            {
-                Console.WriteLine("Categoria: " + g.Categoria + " Quantidade: " + g.NroPessoas);
-
-                foreach (var p in g.Pessoas)
-                    Console.WriteLine("  " + p.Nome);
-            }
-            Console.WriteLine();
-            #endregion
-
-            #region Console.WriteLine("\n>>> Total de pessoas casadas");
-            var r9 = (from p in pessoas
-                      where p.Casada
-                      select p).Count();
-            Console.WriteLine(r9);
-            Console.WriteLine();
-            #endregion
-
-            Console.WriteLine("\n>>> Pessoa mais velha");
+            Console.WriteLine("\n>>> Pessoa mais idosa");
             var r10 = (from p in pessoas
                        orderby p.DataNascimento ascending
                        select p).First();
@@ -220,6 +246,7 @@ namespace Lab11_LinqtoObjects
                        where !p.Casada
                        select p).First();
             Console.WriteLine(r11);
+     
             var maiorIdadeA = pessoas.Min(q => q.DataNascimento);
             Console.WriteLine("\n>>> Pessoa + idosa");
             var r12 = (from p in pessoas          
